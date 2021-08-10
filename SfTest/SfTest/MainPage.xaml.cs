3c9c154ff6de18dcdae01d87b372b2ce2b097810
Diego Martinez <diego.martinez@mayasoft.com.co>
2021-08-10 08:53:33 -0500

    First Commit

---
+++ SfTest/SfTest/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SfTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Calendar.NumberOfWeeksInView = Calendar.NumberOfWeeksInView == 1 ? 6 : 1;
+            if (Calendar.NumberOfWeeksInView == 1)
+            {
+                Calendar.MoveToDate = Calendar.SelectedDate.Value;
+                Calendar.NavigateTo(Calendar.SelectedDate.Value);
+            }
+        }
+    }
+}
