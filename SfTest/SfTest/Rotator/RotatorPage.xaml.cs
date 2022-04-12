e208771268a97ac01e2b3768cbaaa4b2e5f846fd
Diego Martinez <martinez@media-interactive.de>
2022-04-12 17:25:50 -0500

    Add rotator example

--- SfTest/SfTest/Rotator/RotatorPage.xaml.cs
+++ SfTest/SfTest/Rotator/RotatorPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using SfTest.ViewModel;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,10 +12,48 @@ namespace SfTest.Rotator
         public RotatorPage()
         {
             InitializeComponent();
+            BindingContext = new RotatorViewModel();
         }
     }
 
-    public class ItemModel
+    public class RotatorViewModel : BaseViewModel
+    {
+        private List<RotatorItemModel> items = new List<RotatorItemModel>();
+        public List<RotatorItemModel> Items
+        {
+            get { return items; }
+            set { items = value; NotifyPropertyChanged(); }
+        }
+
+        public RotatorViewModel()
+        {
+            Items = new List<RotatorItemModel>()
+            {
+                new RotatorItemModel()
+                {
+                    Name = "Name 0001"
+                    , Description = "Description 0001"
+                }
+                ,new RotatorItemModel()
+                {
+                    Name = "Name 0002"
+                    , Description = "Description 0002"
+                }
+                ,new RotatorItemModel()
+                {
+                    Name = "Name 0003"
+                    , Description = "Description 0003"
+                }
+                ,new RotatorItemModel()
+                {
+                    Name = "Name 0004"
+                    , Description = "Description 0004"
+                }
+            };
+        }
+    }
+
+    public class RotatorItemModel
     {
         public string Name { get; set; }
         public string Description { get; set; }
